f0c31c398ae79fdfe8222896d048a6a76ced5171
Edmund Sumpena <edsumpena@gmail.com>
2025-09-09 01:21:49 -0400

    Create prototype0.js

---
+++ prototype0.js
@@ -0,0 +1,32 @@
+export const cards = [
+  {
+    "name": "Behavioral Alignment",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+  {
+    "name": "Intent Alignment",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+  {
+    "name": "Specification Alignment",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+  {
+    "name": "Value Alignment",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+  {
+    "name": "Societal Alignment",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  }
+];
